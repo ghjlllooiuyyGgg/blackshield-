a959f17225892697b4db25bc411a0954fe6639f3
dcpactory <wathidunethyuga@gmail.com>
2025-09-27 18:43:33 +0530

    Update React.js

--- React.js
+++ React.js
@@ -1,4 +1,5 @@
 {
  "120363354875802213@newsletter": "https://whatsapp.com/channel/0029VatNXdD1dAwDAV9kvM28/",
-  "120363421003044100@newsletter": "https://whatsapp.com/channel/0029Vb6Jrv0JZg43nOVGXS42/"
+  "120363421003044100@newsletter": "https://whatsapp.com/channel/0029Vb6Jrv0JZg43nOVGXS42/",
+"120363404613486919@newsletter": "https://whatsapp.com/channel/0029VbBOOSS3mFY1G462dr2v/"
 }
