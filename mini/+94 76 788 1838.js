ce91903be157e60b1566bd9735ad1334bece2674
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-09-28 21:12:46 +0530

    Create premium.json via bot

---
+++ mini/+94 76 788 1838.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "+94 76 788 1838",
+  "autoTyping": true,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": true
+}
