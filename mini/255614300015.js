23d26182980828221b120781e88f781a5838a97a
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-12 22:28:14 +0530

    Create premium.json via bot

---
+++ mini/255614300015.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "255614300015",
+  "autoTyping": false,
+  "autoRecording": true,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": true
+}
