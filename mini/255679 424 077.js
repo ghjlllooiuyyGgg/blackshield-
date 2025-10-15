7295d7ddc3022c4c97dbbc36dbed25bf4a7d2ca3
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-15 17:32:44 +0530

    Create premium.json via bot

---
+++ mini/255679 424 077.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "255679 424 077",
+  "autoTyping": true,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": true
+}
