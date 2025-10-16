b0d9d8329a9283117e1b26013a1bb1d5034270d3
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-16 06:34:36 +0530

    Create premium.json via bot

---
+++ mini/+94 76 857 9834.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "+94 76 857 9834",
+  "autoTyping": false,
+  "autoRecording": true,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": true
+}
