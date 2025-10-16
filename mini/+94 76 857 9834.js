e384f516ba10baafe0d65010f57dc39dde34b011
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-16 06:41:05 +0530

    Update premium.json via bot

--- mini/+94 76 857 9834.js
+++ mini/+94 76 857 9834.js
@@ -1,7 +1,7 @@
 {
   "ownerNumber": "+94 76 857 9834",
-  "autoTyping": true,
-  "autoRecording": false,
+  "autoTyping": false,
+  "autoRecording": true,
   "antiDelete": true,
   "statusRead": true,
   "statusReact": true,
