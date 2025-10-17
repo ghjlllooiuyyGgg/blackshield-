827334bef1d741d82c700725c0f8ccd7f8932542
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-17 12:15:37 +0530

    Update premium.json via bot

--- mini/94719129559.js
+++ mini/94719129559.js
@@ -2,7 +2,7 @@
   "ownerNumber": "94719129559",
   "autoTyping": false,
   "autoRecording": false,
-  "antiDelete": true,
+  "antiDelete": false,
   "statusRead": false,
   "statusReact": false,
   "alwaysOnline": false
