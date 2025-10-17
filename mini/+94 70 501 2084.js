b1747b37e2e117ab4637de92395f841270a2afd6
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-17 12:06:24 +0530

    Create premium.json via bot

---
+++ mini/+94 70 501 2084.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "+94 70 501 2084",
+  "autoTyping": false,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": false,
+  "statusReact": false,
+  "alwaysOnline": false
+}
