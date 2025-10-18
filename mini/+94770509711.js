17bdb1cec71af5532c2ebe4da9d1de9d96b3b8dd
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-18 23:54:11 +0530

    Create premium.json via bot

---
+++ mini/+94770509711.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "+94770509711",
+  "autoTyping": false,
+  "autoRecording": true,
+  "antiDelete": false,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": true
+}
