2e3665cab94addee09edc61252bae4f69f59e353
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-19 16:02:24 +0530

    Update premium.json via bot

--- mini/212724813079.js
+++ mini/212724813079.js
@@ -2,7 +2,7 @@
   "ownerNumber": "212724813079",
   "autoTyping": false,
   "autoRecording": true,
-  "antiDelete": true,
+  "antiDelete": false,
   "statusRead": true,
   "statusReact": true,
   "alwaysOnline": true
