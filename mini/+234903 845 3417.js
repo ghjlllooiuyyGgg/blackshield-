f7240a2e77dc30d9e85a4180b871cc04ee174669
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-21 04:13:36 +0530

    Create premium.json via bot

---
+++ mini/+234903 845 3417.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "+234903 845 3417",
+  "autoTyping": false,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": false
+}
