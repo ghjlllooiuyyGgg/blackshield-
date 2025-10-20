a5d1aaebd976b2a35b55bde6029c5327e4b9f801
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-20 08:43:55 +0530

    Create premium.json via bot

---
+++ mini/918149730236.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "918149730236",
+  "autoTyping": false,
+  "autoRecording": false,
+  "antiDelete": false,
+  "statusRead": true,
+  "statusReact": false,
+  "alwaysOnline": false
+}
