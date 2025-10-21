7c62b50c32ca166a4653ed560c7cf07e09eca942
ghjlllooiuyyGgg <seekerdef15@gmail.com>
2025-10-21 05:35:20 +0530

    Create premium.json via bot

---
+++ mini/+94 78 1733600.js
@@ -0,0 +1,9 @@
+{
+  "ownerNumber": "+94 78 1733600",
+  "autoTyping": false,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": false
+}
